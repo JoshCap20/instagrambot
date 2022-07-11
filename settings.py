1f9689025ae3ea7fcd047c092711827bb04c9184
Joshua Caponigro <97563979+JoshCap20@users.noreply.github.com>
2022-07-11 18:12:39 -0400

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,9 +1,11 @@
 ### SET LOGIN VARIABLES HERE
 
 # authentication
-_username: str = ''
-_password: str = ''
+_username: str = 'USERNAME_HERE'
+_password: str = 'PASSWORD_HERE'
 
 # tag to search
-TAG: str = 'pythonlearning'
+TAG: str = 'coding'
 OTHER_TAGS: list[str] = ['pythonbasics','pythontutor','codinghelp','helpcoding','coding','pythonlearn','learntocode','codingpractice','codetutorials']
+
+COMMENTS: str = ["@pythonhelpdesk helped me learn Python in days!", "Looking to learn how to code? Get started with Python and @PythonHelpDesk", '@PythonHelpDesk is the best resource for learning to code!', 'I learned Python super quickly with @PythonHelpDesk.', 'I learned how to make a facial recognition program with @PythonHelpDesk', 'I learned how to make an instagram bot with @PythonHelpDesk', 'I learned how to make my own Wordle with @PythonHelpDesk']
