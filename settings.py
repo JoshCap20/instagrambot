aeb5b217f8baed2f1835a8074e7f4ca7535afa10
Joshua Caponigro <97563979+JoshCap20@users.noreply.github.com>
2022-07-11 19:10:10 -0400

    Update settings.py

--- settings.py
+++ settings.py
@@ -4,8 +4,6 @@
 _username: str = 'USERNAME_HERE'
 _password: str = 'PASSWORD_HERE'
 
-# tag to search
-TAG: str = 'coding'
-OTHER_TAGS: list[str] = ['pythonbasics','pythontutor','codinghelp','helpcoding','coding','pythonlearn','learntocode','codingpractice','codetutorials']
-
 COMMENTS: str = ["@pythonhelpdesk helped me learn Python in days!", "Looking to learn how to code? Get started with Python and @PythonHelpDesk", '@PythonHelpDesk is the best resource for learning to code!', 'I learned Python super quickly with @PythonHelpDesk.', 'I learned how to make a facial recognition program with @PythonHelpDesk', 'I learned how to make an instagram bot with @PythonHelpDesk', 'I learned how to make my own Wordle with @PythonHelpDesk']
+
+ACCOUNTS: str = ['python','code','coding','software','tech']
