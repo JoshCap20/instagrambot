e1e8ff5d9ec00fb1a05fedba4642633b4caeba21
Joshua Caponigro <97563979+JoshCap20@users.noreply.github.com>
2022-07-11 23:16:24 -0400

    Update main.py

--- main.py
+++ main.py
@@ -9,13 +9,14 @@ from selenium.webdriver import ActionChains
 from settings import _username, _password, COMMENTS, ACCOUNTS
 from random import randint
 
+
 class AccountInstagramBot:
-    """Follows accounts specified in settings"""
+    """Follows accounts specified in settings."""
     accounts: list[str] = ACCOUNTS
     related_accounts: list[str] = []
-    accounts_commented_on: list[str] = []
-    posts_commented_on: list[str] = []
     successful_comments: dict[str: str] = {}
+    related_account_amount: int = 20
+    comment_counter: int = 0
 
     
     def __init__(self):
@@ -24,19 +25,25 @@ class AccountInstagramBot:
         self.wait = WebDriverWait(self.driver, 10)
         if self.instagram_authentication():
             for account in self.accounts:
-                i = 0
-                self.search(account)
+                self.related_accounts.clear()
+                i: int = 0
+                self.related_accounts = self.search(account)
                 for i in range(len(self.related_accounts)):
                     self.get_account(i)
                     post = self.grab_post()
                     self.comment(account, post)
                     self.driver.get("https://instagram.com")
-                    print(f"[{self.accounts.index(account)}/{len(self.accounts)}] Finished with {account}")
+                    sleep(2)
+                    print(f"{i+1}/{len(self.related_accounts)} for {account}")        
                     i += 1
+                print(f"[{self.accounts.index(account)+1}/{len(self.accounts)}] Finished with {account}")
             print("Finished running bot.")
+            print(self.successful_comments)
             self.driver.quit()
+        else:
+            print("Authentication error")
 
-    def instagram_authentication(self):
+    def instagram_authentication(self, retry=True):
         try:
             self.driver.get("https://instagram.com")
             self.wait.until(EC.presence_of_element_located((By.NAME, 'username'))).send_keys(_username)
@@ -45,10 +52,14 @@ class AccountInstagramBot:
             sleep(1)
             self.wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="loginForm"]/div/div[3]/button/div'))).click()
             print(f"{_username} authenticated successfully")
-            sleep(7)
+            sleep(4)
+            if self.driver.current_url == "https://instagram.com":
+                if not retry:
+                    return False
+                sleep(10)
+                self.instagram_authentication(retry=False)
             return True
         except:
-            print("Authentication error")
             return False
 
         
@@ -56,21 +67,22 @@ class AccountInstagramBot:
         """Searches for account."""
         links: list[str] = []
         self.wait.until(EC.presence_of_element_located((By.TAG_NAME, 'input'))).send_keys(account)
-        sleep(5)
+        sleep(3)
         searchresults = self.wait.until(EC.presence_of_all_elements_located((By.TAG_NAME, 'a')))
-        sleep(5)
-        for searchresult in searchresults[:10]:
+        sleep(7)
+        for searchresult in searchresults[:self.related_account_amount]:
             links.append(searchresult.get_attribute('href'))
         links = [link for link in links if account in link or account[:4] in link]
-        self.related_accounts = links
         print(links)
+        return links
 
     def get_account(self, i):
         """Grabs account"""
         sleep(3)
-        self.driver.get(self.related_accounts[i])
-        
-        
+        if i <= len(self.related_accounts):
+            self.driver.get(self.related_accounts[i])
+        else:
+            print("Whew! Avoided an index error in self.get_account(i).")
 
     def grab_post(self):
         """Grabs post from account to comment on."""
@@ -78,7 +90,7 @@ class AccountInstagramBot:
         links: list[str] = []
         images = self.wait.until(EC.presence_of_all_elements_located((By.TAG_NAME, 'a')))
         sleep(3)
-        for image in images:
+        for image in images[:9]:
             if 'https://www.instagram.com/p/' in image.get_attribute('href'):
                 links.append(image.get_attribute('href'))
         print(links)
@@ -88,18 +100,32 @@ class AccountInstagramBot:
 
     def comment(self, account, post):
         """Comment post mechanics."""
+        if self.comment_counter != 0 and self.comment_counter % 25 == 0:
+            sleep(120)
         try:
             sleep(2)
+            comment = COMMENTS[randint(0, len(COMMENTS)-1)]
             select_textbox = self.wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, '.\_aamx > .\_abl-'))).click()
-            type_text = self.wait.until(EC.presence_of_element_located((By.XPATH, '//textarea'))).send_keys(COMMENTS[randint(0, len(COMMENTS)-1)])
+            type_text = self.wait.until(EC.presence_of_element_located((By.XPATH, '//textarea'))).send_keys(comment)
             sleep(2)
             submit_button = self.wait.until(EC.element_to_be_clickable((By.XPATH, '//form/button/div'))).click()
-            print("[SUCCESS] Commented")
+            print(f"[SUCCESS] Commented: {comment}")
+            self.comment_counter += 1
             if account not in self.successful_comments:
                 self.successful_comments[account] = post
-            self.accounts_commented_on.append(account)
-            self.posts_commented_on.append(post)
-            print(self.successful_comments, self.accounts_commented_on, self.posts_commented_on)
+            else:
+                prior = self.successful_comments[account]
+                if isinstance(prior, list):
+                    new_list: list[str] = []
+                    for item in prior:
+                        new_list.append(item)
+                    new_list.append(post)
+                    self.successful_comments[account] = new_list
+                else:
+                    new_value: list[str] = []
+                    new_value.append(prior)
+                    new_value.append(post)
+                    self.successful_comments[account] = new_value
         except:
             print("[ERROR] Not commented")
 
